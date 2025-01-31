fixed dataset augmentation error, changed data scaling in loss, added histogram loss, minor changes to 3chansingle enc model

data/dataset.py
@@ -448,7 +448,7 @@ class SEN12OPTMS(data.Dataset):
         self.use_s2 = use_s2
         self.use_s2cr = use_s2cr
         self.use_s1 = use_s1
-        assert mode in ["train", "val"]
+        assert mode in ["train", "val", "test"]
         self.mode = mode
         self.rand_use = rand_use if mode == "train" else 0.0
         # provide number of input channels
@@ -485,6 +485,8 @@ class SEN12OPTMS(data.Dataset):
             train_list = [x for x in train_list if x not in val_list]
             sample_dirs = train_list
         elif mode == "val":
+            sample_dirs = val_list[:500]
+        elif mode == 'test':
             sample_dirs = val_list
 
         for folder in sample_dirs:
@@ -516,7 +518,8 @@ class SEN12OPTMS(data.Dataset):
 
         # sort list of samples
         self.samples = sorted(self.samples, key=lambda i: i['id'])
-
+        if mode=='val':
+            self.samples = self.samples[:500]
         print("loaded", len(self.samples),
               "samples from the sen12ms subset", mode)
         self.augment_rotation_param = np.random.randint(
@@ -530,7 +533,8 @@ class SEN12OPTMS(data.Dataset):
 
     def __getitem__(self, index):
         """Get a single example from the dataset"""
-
+        if self.index >= len(self.samples):
+            self.index = 0
         # get and load sample from index file
         sample = self.samples[index]
         image_clear = self.load_sample_opt(sample['s2'])
@@ -549,21 +553,19 @@ class SEN12OPTMS(data.Dataset):
         #ret['cond_image_sar'] = image_sar
         ret['cond_image'] = torch.cat([image_cloud, image_sar], dim = 0)
         ret['path'] = sample['id']
+        self.index += 1
         return ret
 
     def load_sample_opt(self, path):
 
         img = np.array(Image.open(path)).transpose((2,0,1))
         if self.mode == 'train':
-            if not self.augment_flip_param[self.index // 4] == 0:
-                img = np.flip(img, self.augment_flip_param[self.index // 4])
-            if not self.augment_rotation_param[self.index // 4] == 0:
+            if not self.augment_flip_param[self.index] == 0:
+                img = np.flip(img, self.augment_flip_param[self.index])
+            if not self.augment_rotation_param[self.index] == 0:
                 img = np.rot90(
-                    img, self.augment_rotation_param[self.index // 4], (1, 2))
-            self.index += 1
+                    img, self.augment_rotation_param[self.index], (1, 2))
 
-        if self.index // 4 >= len(self.samples):
-            self.index = 0
 
         image = torch.tensor(img.copy())
         image = image / 255.0

models/loss.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from pandas.core.nanops import nanany
 from torch.autograd import Variable
 from pytorch_msssim import ssim
 # class mse_loss(nn.Module):
@@ -12,22 +13,66 @@ from pytorch_msssim import ssim
 
 
 def mse_loss(output, target):
-    return {'total':F.mse_loss(output, target)}
+    tmp_output = (output * 0.5) + 0.5
+    tmp_target = (target * 0.5) + 0.5
+    #tmp_output = torch.clip(tmp_output, 0, 1)
+    #tmp_target = torch.clip(tmp_target, 0, 1)
+    return {'total':F.mse_loss(tmp_output, tmp_target)}
 
 def l1_loss(output, target):
     return {'total':F.l1_loss(output, target)}
 
+def histogram_loss(output, target, mode='hist', coef = 4, bins = 100):
+    if mode=='simple':
+        mean_out = torch.mean(output.reshape(24,-1), dim=1)
+        std_out = torch.std(output.reshape(24,-1), dim=1)
+        mean_target = torch.mean(target.reshape(24,-1), dim=-1)
+        std_target = torch.std(target.reshape(24,-1), dim=1)
+        return (((mean_out - mean_target)**2)/(coef*(std_out**2 + std_target**2))).mean()
+        #return (F.l1_loss(mean_out, mean_target)['total'] + F.l1_loss(std_out, std_target)['total'])*coef
+    elif mode=='hist':
+        hist_out = torch.histogram(output, bins=torch.linspace(0,1,bins))
+        hist_target = torch.histogram(target, bins=torch.linspace(0,1,bins))
+        return F.l1_loss(hist_out[0], hist_target[0])['total']*coef
+
 def ssim_loss(output, target):
-    ssim_loss = 1 - ssim((output+1)/2, (target+1)/2, data_range=1, size_average=True)
+    """    tmp_output = (output * 0.5) + 0.5
+    tmp_target = (target * 0.5) + 0.5
+    tmp_output = torch.clip(tmp_output,0,1)
+    tmp_target = torch.clip(tmp_target,0,1)"""
+
+    ssim_loss = 1 - ssim(output, target, data_range = 1.0, size_average=True)
     return {'total':ssim_loss}
 
-def ssim_mse_loss(output, target, coef = 5):
+def ssim_mse_loss(output, target, coef = 4):
     """
     coef: rate for mse loss
     """
-    mseloss = mse_loss(output, target)['total']
-    ssimloss = ssim_loss(output,target)['total']* coef
+    tmp_output = (output * 0.5) + 0.5
+    tmp_target = (target * 0.5) + 0.5
+    tmp_output = torch.clip(tmp_output, 0, 1)
+    tmp_target = torch.clip(tmp_target, 0, 1)
+    mseloss = mse_loss(tmp_output, tmp_target)['total']
+    ssimloss = ssim_loss(tmp_output,tmp_target)['total']* coef
     return {'ssim_loss':ssimloss, 'mse_loss':mseloss, 'total': mseloss+ssimloss, 'ssim_mse_loss': mseloss+ssimloss }
+
+def ssim_mse_hist_loss(output, target, coef = 4):
+    """
+    coef: rate for mse loss
+    """
+    tmp_output = (output * 0.5) + 0.5
+    tmp_target = (target * 0.5) + 0.5
+    tmp_output = torch.clip(tmp_output, 0, 1)
+    tmp_target = torch.clip(tmp_target, 0, 1)
+    hist_loss = histogram_loss(tmp_output, tmp_target , mode='simple')
+    mseloss = mse_loss(tmp_output, tmp_target)['total']
+    ssimloss = ssim_loss(tmp_output,tmp_target)['total']* coef
+    if torch.isnan(mseloss+ssimloss+ hist_loss):
+        exit(-1)
+    return {'ssim_loss':ssimloss, 'mse_loss':mseloss, 'histogram_loss': hist_loss,
+            'total': mseloss+ssimloss+ hist_loss, 'ssim_mse_hist_loss': mseloss+ssimloss + hist_loss }
+
+
 
 class FocalLoss(nn.Module):
     def __init__(self, gamma=2, alpha=None, size_average=True):

models/model.py
@@ -3,6 +3,7 @@ import tqdm
 from core.base_model import BaseModel
 from core.logger import LogTracker
 import copy
+from torch.cuda.amp import GradScaler, autocast
 class EMA():
     def __init__(self, beta=0.9999):
         super().__init__()
@@ -24,6 +25,7 @@ class Palette(BaseModel):
         ''' networks, dataloder, optimizers, losses, etc. '''
         self.loss_fn = losses[0]
         self.netG = networks[0]
+        self.scaler = GradScaler(enabled=self.opt['autocast'])
         if ema_scheduler is not None:
             self.ema_scheduler = ema_scheduler
             self.netG_EMA = copy.deepcopy(self.netG)
@@ -68,8 +70,8 @@ class Palette(BaseModel):
     def get_rgb_tensor(self, rgb):
         rgb = rgb*0.5+0.5
         rgb = rgb - torch.min(rgb)
-        print(rgb.min())
-        print(rgb.max())
+        #print(rgb.min())
+        #print(rgb.max())
 
         # treat saturated images, scale values
         if torch.max(rgb) == 0:
@@ -130,9 +132,11 @@ class Palette(BaseModel):
         for train_data in pbar:
             self.set_input(train_data)
             self.optG.zero_grad()
-            loss = self.netG(self.gt_image, self.cond_image, mask=self.mask)
-            loss['total'].backward()
-            self.optG.step()
+            with autocast(enabled=self.opt['autocast'], dtype=torch.float16):
+                loss = self.netG(self.gt_image, self.cond_image, mask=self.mask)
+            self.scaler.scale(loss['total']).backward()
+            self.scaler.step(self.optG)
+            self.scaler.update()
 
             pbar.set_postfix(**loss)
             loss.pop('total')

models/ours/mine_3chan_sar_single_enc.py
@@ -340,10 +340,10 @@ class UNet(nn.Module):
         t = self.map(self.emb(gammas.view(-1, ))) #time embedding with sinusoidal embedding and a simple MLP
         inp = self.check_image_size(inp)
 
-        cond, x = inp[:,:6],inp[:,6:]  #split input into 4 chunks
-        #cond = torch.stack([x1, x2, x3], dim=1)#stack conditional chunks adding additional dimension
-        #b, n, c, h, w = cond.shape
-        #cond = cond.view(b*n, c, h, w)#reshape
+        cloud, sar, x = inp.chunk(3, dim=1)  #split input into 3 chunks
+        cond = torch.stack([cloud, sar], dim=1)#stack conditional chunks adding additional dimension
+        b, n, c, h, w = cond.shape
+        cond = cond.view(b, c*n, h, w)#reshape
         x = self.intro(x) #1x1 convolution
         cond = self.cond_intro(cond)#1x1 convolution
 
@@ -354,7 +354,7 @@ class UNet(nn.Module):
             x = encoder(x, t)
             cond = cond_encoder(cond)
             #b, c, h, w = cond.shape
-            #tmp_cond = cond.view(b//3, 3, c, h, w).sum(dim=1)
+            #tmp_cond = cond.view(b, n, c//n, h, w).sum(dim=1)
             x = x + cond
             encs.append(x)
             x = down(x)